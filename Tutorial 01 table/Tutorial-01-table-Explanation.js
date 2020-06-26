66b53bad18b8a3ce776eab09cd5f7f8a9866a0ac
Shiba-Programming <shibamaruart@gmail.com>
2020-06-27 05:59:36 +0900

    HTML tag]

---
+++ Tutorial 01 table/Tutorial-01-table-Explanation.js
@@ -0,0 +1,6 @@
+console.log("tableは表を意味する英単語table（テーブル）そのままです");
+console.log("HTML上に表を作ることができます");
+
+console.log("trは table row（テーブル・ロウ）の略で、表の1行を表しています");
+console.log("thはtable header（テーブル・ヘッダー）の略で、表の見出しを表しています");
+console.log("tdはtable data（テーブル・データ）の略で、表の中身を表しています");
--- Tutorial 01 ul/Tutorial-01-ul-Explanation.js
+++ Tutorial 01 ul/Tutorial-01-ul-Explanation.js
@@ -5,4 +5,8 @@ console.log("ulはunordered list（アンオーダード・リスト）の略で
 console.log("liタグは　list item（）リストアイテムの略でリストの項目を表すタグです")
 console.log("liタグはulタグの子要素とする必要があります")
 
+console.log("ulタグの代わりにolタグを利用すると順序付きのリストを作ることができる")
+
+console.log("olはodered list （オーダーリスト）の略です")
+
 //説明終了
