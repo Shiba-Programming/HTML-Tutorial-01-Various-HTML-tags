ccb1fd68e1b85d6ee898aa44d5849badec4145d4
Shiba-Programming <shibamaruart@gmail.com>
2020-06-26 23:50:58 +0900

    01

---
+++ Tutorial 01 img/Tutorial-01-img-Explanation.js
@@ -0,0 +1,9 @@
+console.log("imgはimage（イメージ）の略で、画像のこと");
+console.log("このタグは、HTMLに画像を読み込むことができます");
+console.log("画像が読み込まれなかった時に代替のテキスト指定できます");
+
+console.log("alt（オルト）属性はalternate（オルタネート、代替）の略で、代替テキストと呼ばれます");
+
+console.log("画像のサイズ指定をする場合テキストの後に width=(数値) height=(数値)で指定可能");
+
+console.log("widthは幅の指定・heightは高さの指定");
